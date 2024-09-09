961cb3cbb0e56ce5db0320bb94ba7ba60876a9e2
south <south@LAPTOP-EMCE28DA>
2024-09-09 09:28:59 +0200

    First Commit

---
+++ src/main/java/com/mycompany/first_project_git/First_project_Git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.first_project_git;
+
+/**
+ *
+ * @author south
+ */
+public class First_project_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
